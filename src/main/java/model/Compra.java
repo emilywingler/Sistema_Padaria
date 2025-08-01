0927f4d2edce51c054bfa0d561a242bbd4af8fe9
emilywingler <wingleremily@gmail.com>
2025-08-01 00:07:01 -0300

    Classe Compra criada!

---
+++ src/main/java/model/Compra.java
@@ -0,0 +1,45 @@
+
+package model;
+
+/**
+ *
+ * @author emily
+ */
+public class Compra {
+    private final int idCompra;
+    private int idFornecedor;
+    private String DataCompra;
+    private int idProduto;
+    private int quantidade;
+
+    public Compra(int idCompra, int idFornecedor, String DataCompra, int idProduto, int quantidade) {
+        this.idCompra = idCompra;
+        this.idFornecedor = idFornecedor;
+        this.DataCompra = DataCompra;
+        this.idProduto = idProduto;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public String getDataCompra() {
+        return DataCompra;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    
+    
+    
+}
