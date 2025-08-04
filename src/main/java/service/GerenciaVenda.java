1ca3eff10c3cd9ef4466add35dacf376e05e751a
emilywingler <wingleremily@gmail.com>
2025-08-04 14:26:37 -0300

    GerenciaVenda: listarvendas criadas

--- src/main/java/service/GerenciaVenda.java
+++ src/main/java/service/GerenciaVenda.java
@@ -9,6 +9,7 @@ import model.ClienteJuridico;
 import model.Compra;
 import io.Leitura;
 import io.Escrita;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import model.Produto;
@@ -18,24 +19,83 @@ import model.Venda;
 /**
  *
  * @author emily
+ * 
+ * + registrarVenda() : void
++ listarVendas(): void
++ totalAReceberCliente(String codigo) : BigDecimal
++ receitaPorProduto(String codigoProd): BigDecimal
++ lucroPorProduto(String codigoProd): BigDecimal
++ receitaPorMP(char meioPagamento): BigDecimal
++ lucroPorMP(char meioPagamento): BigDecimal
++ filtrarVendasAReceber() : List<Venda>
+* 
+* 
+* Relatórios gerados:
+1-apagar.csv <nome>;<cnpj>;<pessoa contato>;<telefone>;<valor total a pagar>
+2-areceber.csv <nome>;<tipo>;<cpf/cnpj>;<telefone>;<data>;<valor total a receber>
+3-vendasprod.csv <código>;<descrição>;<receita bruta>;<lucro>
+4-vendaspgto.csv <modo de pagamento>;<receita bruta>;<lucro>
+5-estoque.csv <código>;<descrição>;<quantidade>;<observações>
+Observação: "COMPRAR MAIS" se estoque < mínimo.
  */
 public class GerenciaVenda {
-    private List<Venda> vendas; //lista que armazena os objetos Produto em memoria
+    private List<Venda> vendas; //lista que armazena os objetos Venda em memoria
     private final String ARQUIVO_VENDA= "vendas.csv"; //nome do arquivo csv que os produtos sao salvos 
-    private Leitura leitorCSV;//objeto responsavel por lerdados no csv
-    private Escrita escritorCSV;//objeto responsavel por escrever dados no csv
+    private Leitura leitorCSV;//objeto responsavel por ler dados no csv
+    private Escrita escritorCSV;
+    private List<Produto> produtos//objeto responsavel por escrever dados no csv
     
-    public GerenciaVenda() {
+    public GerenciaVenda(List<Produto>) {
         vendas = new ArrayList<>();
         leitorCSV = new Leitura();
-        escritorCSV = new Escrita();
+        escritorCSV = new Escrita();produtos = new ArrayList<>();
+        
     }
     
-    public void registrarVenda(Venda v){
-        
+// USAR o Polimorfismo de INclusão aqui, para simplificar
+    
+// Não tem deletar vendas ou editar vendas, porque cada venda é única;
+    public void registrarVenda(int idVenda, String DataVenda, int idProduto, int quantidade, char MeioPagamento){
+        Produto p;
+        p = buscar
+        Venda v = new Venda(idVenda, DataVenda, idProduto, quantidade, MeioPagamento);
+        vendas.add(v);
+        //escritorCSV.atualizarArquivoVenda(,v);
     }
     
     public void listarVendas(){
+        if(!vendas.isEmpty()){
+            for(Venda v : vendas){
+                System.out.println(v.toString());
+            }
+        }
+    }
+    
+    public Venda buscarVenda(int codigoVenda){
+        if(!vendas.isEmpty()){
+            for(Venda v : vendas){
+                if(v.getIdVenda() == codigoVenda){
+                    return v;
+                }
+                else{
+                    return null;
+                }
+            }
+        }
+    }
+    
+    
+    public BigDecimal receitaPorProduto(String codigoProd){
         
     }
+    public BigDecimal lucroPorProduto(String codigoProd){
+    }
+    
+    public BigDecimal receitaPorMP(char meioPagamento){}
+    public BigDecimal lucroPorMP(char meioPagamento){}
+    
+    
+    public BigDecimal totalAReceberCliente(String codigo){}
+    
+    public List<Venda> filtrarVendasAReceber(){}
 }
